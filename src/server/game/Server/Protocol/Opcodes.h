8d9c31ad0ecb933fd2480767f04db1ff9494f11d
netcho <kkotlarski@abv.bg>
2014-03-12 21:58:10 +0200

    Add an opcode value, it's currently unhandled by the server
    Signed-off-by: netcho <kkotlarski@abv.bg>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -391,7 +391,7 @@ enum Opcodes
    CMSG_NEXT_CINEMATIC_CAMERA                               = 0x0,
    CMSG_NPC_TEXT_QUERY                                      = 0x12FA, // 5.4.7 17956
    CMSG_OBJECT_UPDATE_FAILED                                = 0x0882, // 5.4.7 17956
-   CMSG_OBJECT_UPDATE_RESCUED                               = 0x0,
+   CMSG_OBJECT_UPDATE_RESCUED                               = 0x042E, // 5.4.7 17956
    CMSG_OFFER_PETITION                                      = 0x0,
    CMSG_OPENING_CINEMATIC                                   = 0x0,
    CMSG_OPEN_BROWSER                                        = 0x0,
