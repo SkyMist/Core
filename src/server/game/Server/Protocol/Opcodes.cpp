10242c1696ee4c3aca3b5d41d1ad3894871cee55
netcho <kkotlarski@abv.bg>
2014-03-16 23:10:08 +0200

    Updated and enabled QUERY_TIME opcodes, this should fix the TimeManager issue
    Signed-off-by: netcho <kkotlarski@abv.bg>

--- src/server/game/Server/Protocol/Opcodes.cpp
+++ src/server/game/Server/Protocol/Opcodes.cpp
@@ -425,7 +425,7 @@ void OpcodeTable::InitializeClientTable()
     DEFINE_OPCODE_HANDLER(CMSG_QUERY_GUILD_XP,                          STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleGuildQueryXPOpcode        ); // STATUS_UNHANDLED
     DEFINE_OPCODE_HANDLER(CMSG_QUERY_INSPECT_ACHIEVEMENTS,              STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQueryInspectAchievements  );
     DEFINE_OPCODE_HANDLER(CMSG_QUERY_QUESTS_COMPLETED,                  STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQueryQuestsCompleted      );
-    DEFINE_OPCODE_HANDLER(CMSG_QUERY_TIME,                              STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQueryTimeOpcode           );
+    DEFINE_OPCODE_HANDLER(CMSG_QUERY_TIME,                              STATUS_LOGGEDIN,     PROCESS_THREADSAFE,   &WorldSession::HandleQueryTimeOpcode           );
     DEFINE_OPCODE_HANDLER(CMSG_QUESTGIVER_ACCEPT_QUEST,                 STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQuestgiverAcceptQuestOpcode);
     DEFINE_OPCODE_HANDLER(CMSG_QUESTGIVER_CHOOSE_REWARD,                STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQuestgiverChooseRewardOpcode);
     DEFINE_OPCODE_HANDLER(CMSG_QUESTGIVER_COMPLETE_QUEST,               STATUS_UNHANDLED,    PROCESS_THREADUNSAFE, &WorldSession::HandleQuestgiverCompleteQuest   );
