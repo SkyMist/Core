c8cb238fe46070a8919c199d9c53e4cce1a44b3c
cocolino <qler16@yahoo.com>
2014-04-03 01:01:07 +0300

    Server/NetworkIO: Implemented basic Guilds system opcodes and updated a few others.

--- src/server/game/Handlers/AuthHandler.cpp
+++ src/server/game/Handlers/AuthHandler.cpp
@@ -20,9 +20,12 @@
 #include "Opcodes.h"
 #include "WorldSession.h"
 #include "WorldPacket.h"
+#include "Config.h"
 
 void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
-{   
+{
+    std::map<uint32, std::string> realmNamesToSend;
+
     QueryResult classResult = LoginDatabase.PQuery("SELECT class, expansion FROM realm_classes WHERE realmId = %u", realmID);
     QueryResult raceResult = LoginDatabase.PQuery("SELECT race, expansion FROM realm_races WHERE realmId = %u", realmID);
 
@@ -32,6 +35,10 @@ void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
         return;
     }
 
+    RealmNameMap::const_iterator iter = realmNameStore.find(realmID);
+    if (iter != realmNameStore.end()) // Add local realm
+        realmNamesToSend[realmID] = iter->second;
+
     TC_LOG_ERROR("network", "SMSG_AUTH_RESPONSE");
     WorldPacket packet(SMSG_AUTH_RESPONSE, 80);
     
@@ -39,7 +46,15 @@ void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
 
     if (code == AUTH_OK)
     {
-        packet.WriteBits(0, 21);
+        packet.WriteBits(realmNamesToSend.size(), 21); // Send current realmId
+
+        for (std::map<uint32, std::string>::const_iterator itr = realmNamesToSend.begin(); itr != realmNamesToSend.end(); itr++)
+        {
+            packet.WriteBits(itr->second.size(), 8);
+            packet.WriteBit(itr->first == realmID); // Home realm
+            packet.WriteBits(itr->second.size(), 8);
+        }
+
         packet.WriteBit(0);
         packet.WriteBits(raceResult->GetRowCount(), 23);
         packet.WriteBits(0, 21);
@@ -83,6 +98,13 @@ void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
         while (raceResult->NextRow());
 
         packet << uint32(Expansion());
+
+        for (std::map<uint32, std::string>::const_iterator itr = realmNamesToSend.begin(); itr != realmNamesToSend.end(); itr++)
+        {
+            packet << uint32(itr->first);
+            packet.WriteString(itr->second);
+            packet.WriteString(itr->second);
+        }
 
         packet << uint32(0);
         packet << uint32(0);
