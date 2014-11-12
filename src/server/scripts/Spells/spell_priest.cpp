aae22a76d95ab5ed16751dc9c99c02d7f1b67ac1
foo <foo@foo.com>
2014-11-12 00:33:52 -0800

    fixed a typo

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -3279,7 +3279,7 @@ void AddSC_priest_spell_scripts()
     new spell_pri_divine_insight_discipline();
     new spell_pri_holy_word_sanctuary();
     new spell_pri_chakra_chastise();
-    //new spell_pri_lightwell_renew();
+    new spell_pri_lightwell_renew();
     new spell_pri_strength_of_soul();
     new spell_pri_grace();
     new spell_pri_train_of_thought();
